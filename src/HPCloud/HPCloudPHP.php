a15ec250c1bb2189f66c723453398ba77db2e6dd
john5037 <john5037>
2013-11-25 16:06:29 +0530

    change in file

--- src/HPCloud/HPCloudPHP.php
+++ src/HPCloud/HPCloudPHP.php
@@ -13,12 +13,11 @@
 namespace HPCloud;
 
 require_once 'Bootstrap.php';
-  
+
 use HPCloud\Bootstrap;
 use HPCloud\Services\IdentityServices;
 use HPCloud\Storage\ObjectStorage;
 use HPCloud\Storage\ObjectStorage\Object;
-
 
 class HPCloudPHP {
 	
@@ -47,7 +46,7 @@ class HPCloudPHP {
 	 */      	
 	 
 	private $streamTransport = "\HPCloud\Transport\PHPStreamTransport"; 
-	/**
+ /**
 	* Variable:	$token
 	* Description:	The token ID for REST calls
 	*/
@@ -75,8 +74,8 @@ class HPCloudPHP {
 	 * Varaible: $mime
 	 * Description: MimeType 	 
 	 * 
-	**/
-  private $mime = 'image/jpeg';      	
+	 **/
+ private $mime = 'image/jpeg';      	
   
  /**
 	* Function:	HPCloud()
@@ -86,15 +85,14 @@ class HPCloudPHP {
 	*/
 	function __construct($account=null,$secreat=null,$tenantId=null,$identity_url=null,$stream_transport=null) 
 	{
-	  
     if($identity_url == null)
-      $identity_url = $this->identity_url;
+        $identity_url = $this->identity_url;
       
     if($stream_transport == null)
-      $streamtransport= $this->streamTransport;
+        $streamtransport= $this->streamTransport;
       
     Bootstrap::useAutoloader();
-    Bootstrap::useStreamWrappers();
+  //  Bootstrap::useStreamWrappers();
      
     $this->setConfigurations($account,$secreat,$tenantId,$identity_url,$streamtransport);  
     
@@ -217,42 +215,189 @@ class HPCloudPHP {
     
     if($subDir != null)
        $fileName = $subDir."/".$fileName;
-         
+    
     $localObject = new Object($fileName,file_get_contents($fileContent,$this->mime));
-    $container->save($localObject);
+    $respnse = $container->save($localObject);
+    return $respnse;
   }
+ 
+ /**
+	 * Creates an HP Cloud S3 object. After an HP Cloud Container is created, objects can be stored in it.
+ 	 */
+ public function create_object($container=null,$filename=null,$opt = null)
+ {
+ 
+    $subDir = '';$bodyFlag=0;
+    if(isset($opt['encryption'])) {
+   
+    }
+  	if (isset($opt['contentType']))
+		{
+			$mime = $opt['contentType'];
+  		unset($opt['contentType']);
+		}
+	
+		if(isset($opt['subDir'])) {
+      $subDir = $opt['subDir'];
+      unset($opt['subDir']);
+    }
+    if(isset($opt['fileUpload'])) {
+      $fileContent = $opt['fileUpload'];
+      $fileContents = '';
+      if ($fileContent) {
+          while (($buffer = fgets($fileContent, 4096)) !== false) {
+              $fileContents .= $buffer;
+          }
+          if (!feof($fileContent)) {
+              echo "Error: unexpected fgets() fail\n";
+          }
+          fclose($fileContent);
+      }     
+      file_put_contents($filename, $fileContents);  
+      unset($opt['fileUpload']);
+    }
+  	if (isset($opt['body']))
+		{
+			$fileContent = $opt['body'];
+	  	file_put_contents($filename, $fileContent);  
+      $bodyFlag=1;    	
+			unset($opt['body']);
+		}  
+    //echo $fileContent;exit;
+    
+		if($bodyFlag==1)	 
+       $response = $this->SaveToObjectStorage($container,$filename,$filename,$subDir,$mime);
+    else
+      $response = $this->SaveToObjectStorage($container,$filename,$filename,$subDir,$mime); 
+ } 
+  /**
+	 * Gets a simplified list of HP Cloud object file names contained in a container.
+   */
+ public function get_object_list($container=null,$opt=null)
+ {
+   
+   $catalog = $this->identity->serviceCatalog('object-store');
+   $store = ObjectStorage::newFromServiceCatalog($catalog, $this->token);
+   $ContainerObj=$store->container($container);
+   $result = $ContainerObj->objects();
+   return $result;
+ }
+ /**
+	 * Copies an HP Cloud S3 object to a new location, whether in the same HP Cloud region, container, or otherwise
+	 * $source It Must be Object of Class object    
+	 *  copy from private bucket to public bucket	 
+   */	 
+ public function copy_object($source=array(),$dest=array(),$opt=null)
+ {
+   $catalog = $this->identity->serviceCatalog('object-store');
+   $store = ObjectStorage::newFromServiceCatalog($catalog, $this->token);
+   $container = $source['bucket'];
+   $ContainerObj=$store->container($container);
+   $obj = $this->get_object($source['bucket'],$source['filename']);
+   
+   $result = $ContainerObj->copy($obj,$dest['filename'],$dest['bucket']);
+   
+   return $ContainerObj;
+ }
+  /**
+	 * Deletes one or more specified Hp Cloud S3 objects from the specified container.
+   */
+ public function delete_objects($container, $fileitems,$opt=null)
+ {
 
+ }
+  /**
+	 * Gets the contents of an HP Cloud S3 object in the specified Container.
+	 * example $filename = 	 search_index/20fa40b5dbc7d2e77273969d9b67c921.json
+	 */
+ public function get_object($container=null,$filename=null,$opts=null)
+ {
+   $catalog = $this->identity->serviceCatalog('object-store');
+   $store = ObjectStorage::newFromServiceCatalog($catalog, $this->token);
+   $ContainerObj=$store->container($container);
+   $result = $ContainerObj->object($filename);
+   return $result;
+ }
+ /**
+	 * Deletes an HP Cloud S3 object from the specified container.
+	 */
+ public function delete_object($container=null,$filename=null,$opt=null)
+ {
+   $catalog = $this->identity->serviceCatalog('object-store');
+   $store = ObjectStorage::newFromServiceCatalog($catalog, $this->token);
+   $ContainerObj=$store->container($container);
+   $data = $ContainerObj->delete($filename);
+   
+ }
+ 
+ /**
+	 * Gets whether or not the specified HP Cloud S3 object exists in the specified Container.
+	 * return true if find	 
+	 */
+ public function if_object_exists($container, $filename)
+ {
+ 
+ }
+ 
+ /**
+	 * Gets the web-accessible URL for the HP Cloud S3 object or generates a time-limited signed request for
+	 * a private file.
+	 */
+ public function get_object_url($container,$filename)
+ {
+ 
+ }
+ 
  public function faceDetection($filename='',$url_object_store='')
  {
        
     $url_pic = $filename;
-    
     $query_str = "url_pic=".$url_pic."&url_object_store=".$url_object_store."&filename=j1.jpg";
-    
-    //$url = "http://map-api.hpl.hp.com/facedetect".$query_str;
     $url = "http://map-api.hpl.hp.com/facedetect?".$query_str;
-    
     $ch = curl_init($url);
     
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  'X-Auth-Token: ' . $this->token,
-  
-  'mime: image/jpeg'  
-));
+      'X-Auth-Token: ' . $this->token,
+      'mime: image/jpeg'  
+    ));
 
     //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
     $response = curl_exec($ch);
-
     if($response === false)
     {
       echo 'Curl error: ' . curl_error($ch);die();
     }
-      return $response;  
+    return $response;  
  }
- 
+  public function faceVerification($filename='',$url_object_store='',$gallary_url = array())
+  {
+    $url_pic_source = $filename;
+    $url_pic = "";
+    foreach($gallary_url as $gallary){
+        $url_pic .= "&url_pic=".$gallary;
+    }
+    $url_object_store = "&url_object_store=".$url_object_store;
+
+    $query_str = "url_pic_source=".$url_pic_source.$url_pic.$url_object_store;
+
+    $url = "http://map-api.hpl.hp.com/faceverify?".$query_str;
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'X-Auth-Token: ' . $this->token,
+    ));
+    $response = curl_exec($ch);
+    if($response === false)
+    {
+      echo 'Curl error: ' . curl_error($ch);die();
+    }
+    
+     return $response;
+ }
+  
  private function addQueue($queue_name='',$messaging_url='')
  {
+ 
     if($messaging_url == '')
             $messaging_url = $this->messaging_url.$this->tenantId;
          
@@ -301,10 +446,12 @@ class HPCloudPHP {
  
     // First we have to check that queue is Exist or not if its not exist then we have to create the queue
     $this->checkQueue($queue_name);
+    
     if($messaging_url == '')
             $messaging_url = $this->messaging_url.$this->tenantId;
     
     $url = $messaging_url.'/queues/'.$queue_name.'/messages';
+    
     $ch = curl_init($url);
     
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -318,6 +465,7 @@ class HPCloudPHP {
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
    
     $response = curl_exec($ch);
+   
     if($response === false)
     {
       echo 'Curl error: ' . curl_error($ch);die();
@@ -326,9 +474,11 @@ class HPCloudPHP {
  }
  public function getMessageFromQueue($queue_name='',$messaging_url='')
  {
+   
    if($messaging_url == '')
             $messaging_url = $this->messaging_url.$this->tenantId;   
     $url = $messaging_url.'/queues/'.$queue_name.'/messages';
+    
     $ch = curl_init($url);   
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'X-Auth-Token: ' . $this->token,
